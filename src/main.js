f4176070d0ef1a08c2cb598faf62e8accd632b49
RITUIYI <1500381663@qq.com>
2019-09-22 14:29:24 +0800

    项目文件创建完成
    项目文件创建完成

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/'
+import store from './store/'
+
+//导入Vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
+//导入适配插件
+import 'postcss-pxtorem'//导入 并在postcss.config.js 进行配置
+import 'amfe-flexible/' //导入即可  动态匹配当前设备宽度
 
 Vue.config.productionTip = false
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="loginBar">
+      登录组件
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'login',
+
+}
+</script>
+
+<style lang='less' scoped>
+    .loginBar {
+        width: 375px;
+        height: 200px;
+        background-color: aquamarine;
+        font-size: 12px;
+    }
+</style>
